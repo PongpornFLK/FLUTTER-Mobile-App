dc1517f8a163a515ca8276bc06adaf78990cf3fb
PongpornFLK <pongporn.yt2910@gmail.com>
2024-12-18 13:57:30 +0700

    Edit code week2

--- week2_begin_flutter/lib/main.dart
+++ week2_begin_flutter/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   const MyApp = MaterialApp(
     title: "Kmutnb",
-    home: Text("Application"),
+    home: Text("App"),
   );
   runApp(MyApp);
 }
