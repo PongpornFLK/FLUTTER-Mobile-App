c15bae38832df663cb135856f7bb838fa93b780d
PongpornFLK <pongporn.yt2910@gmail.com>
2024-12-21 22:26:00 +0700

    Decorate : add gradient , boxshadow , others..

--- week4_assign_car_charging_ui/lib/main.dart
+++ week4_assign_car_charging_ui/lib/main.dart
@@ -23,7 +23,6 @@ class Appme extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Image.asset("assets/images/evlogo5.png", width: 60),
-          
           actions: [
             IconButton(
                 onPressed: () {
@@ -40,6 +39,16 @@ class Appme extends StatelessWidget {
               },
               icon: const Icon(
                 Icons.account_circle,
+                size: 32,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {
+                debugPrint("My Profile");
+              },
+              icon: const Icon(
+                Icons.settings_sharp,
                 size: 32,
                 color: Colors.white,
               ),
@@ -75,17 +84,21 @@ class Appme extends StatelessWidget {
                   ),
                   alignment: Alignment.center,
                   decoration: BoxDecoration(
-                    color: const Color.fromARGB(67, 173, 181, 189),
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 0.01,
-                      style: BorderStyle.solid,
-                    ),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          blurRadius: 15,
+                          spreadRadius: 8,
+                        )
+                      ]),
                 ),
-                SizedBox(height: 14),
-                Image.asset("assets/images/ora-car2.png",width: 300),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 0),
+                  child: Image.asset("assets/images/ora-car2.png", width: 300),
+                ),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
@@ -202,7 +215,7 @@ class Appme extends StatelessWidget {
                             ),
                             borderRadius: BorderRadius.circular(20),
                           ),
-                          hintText: "Bat Capacity kWh",
+                          hintText: "Bat Capacity .kWh",
                         ),
                         style:
                             TextStyle(fontSize: 15, fontFamily: "Montserrat"),
@@ -230,7 +243,6 @@ class Appme extends StatelessWidget {
                     ),
                   ],
                 ),
-
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
@@ -249,77 +261,58 @@ class Appme extends StatelessWidget {
                       height: 150.0,
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 13, 27, 42),
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 245, 188, 74),
-                            width: 0.01,
-                            style: BorderStyle.solid,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              blurRadius: 15,
-                              spreadRadius: 8,
-                            )
-                          ]),
+                        gradient: LinearGradient(colors: <Color>[
+                          const Color(0xFF7ef29d),
+                          const Color(0xFF099773),
+                        ], begin: Alignment.topLeft),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text("Charging Power",
                               style: TextStyle(
-                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.w900,
                                   color: Colors.white)),
-                          Icon(Icons.power, size: 72, color: Colors.white),
+                          Icon(Icons.bolt, size: 56, color: Colors.white),
                           Text("-------",
                               style: TextStyle(
                                   fontFamily: "Montserrat",
                                   color: Colors.white)),
                           Text("kWh",
                               style: TextStyle(
-                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.w900,
                                   color: Colors.white)),
                         ],
                       ),
                     ),
-
-                    
                     Container(
                       margin: const EdgeInsets.all(10),
                       width: 160.0,
                       height: 150.0,
                       alignment: Alignment.center,
                       decoration: BoxDecoration(
-                          color: const Color.fromARGB(255, 13, 27, 42),
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 245, 188, 74),
-                            width: 0.01,
-                            style: BorderStyle.solid,
-                          ),
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              blurRadius: 15,
-                              spreadRadius: 8,
-                            )
-                          ]),
+                        gradient: LinearGradient(colors: <Color>[
+                          const Color(0xFFFFD700),
+                          const Color(0xFFff930f)
+                        ], begin: Alignment.topLeft),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
                           Text("Charging Time",
                               style: TextStyle(
-                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.w900,
                                   color: Colors.white)),
-                          Icon(Icons.access_time_filled,
-                              size: 72, color: Colors.white),
+                          Icon(Icons.av_timer, size: 56, color: Colors.white),
                           Text("-------",
                               style: TextStyle(
                                   fontFamily: "Montserrat",
                                   color: Colors.white)),
                           Text("hrs",
                               style: TextStyle(
-                                  fontFamily: "Montserrat",
+                                  fontWeight: FontWeight.w900,
                                   color: Colors.white)),
                         ],
                       ),
@@ -327,20 +320,25 @@ class Appme extends StatelessWidget {
                   ],
                 ),
                 Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 8 , horizontal: 24),
-                  child: Column(crossAxisAlignment: CrossAxisAlignment.stretch,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 90),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
                       ElevatedButton(
                         child: Text(
                           "CALCULATE",
                           style: TextStyle(
-                              color: Colors.white, fontWeight: FontWeight.bold , fontFamily: "OpenSans" , fontSize: 18),
+                              color: Colors.white,
+                              fontWeight: FontWeight.w900,
+                              fontSize: 16),
                         ),
                         onPressed: () {
                           (debugPrint("Calculating"));
                         },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF2b9348),
+                          backgroundColor:
+                              const Color.fromARGB(255, 19, 20, 20),
                         ),
                       ),
                     ],
